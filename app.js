bd0ad51697cce8d30ec4de2a630c7ab27c2eb5c4
Chaz Corlew <chaz.corlew@gmail.com>
2022-12-14 01:05:33 -0800

    have added partials, and added CSS to site, more functionality to be added

--- app.js
+++ app.js
@@ -2,32 +2,38 @@ const express = require('express');
 
 // express app
 const app = express();
-app.disable('etag');
+
+//register vire engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-// res.send('<p>home page</p>');
-res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: "Chainsaw Man is the best anime out right now and it isn't close", snippet: 'duh duh duh duh duh duh'},
+        {title: "How My Hero Academia saved my life...", snippet: 'Midoriya was just a kid who wanted to be like his heroes'},
+        {title: "The best anime swords to start your anime sword collection", snippet: 'The answer is Demon Slayer, obvi...'},
+       ];
+
+    res.render('index', {title: 'Home', blogs });
+
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', {title: 'About'});
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'Create a New Rant'});
+})
 
 
 // 404 page
 
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname })
+res.status(404).render('404', { title: 'Aww Crap'});
 });
-
 
 
 
